da00d3a8afa081c01934810cc23adf21353a6f71
Michael Schwartze <mschwartze@gmail.com>
2016-10-21 14:28:40 -0400

    Create Student.learn and Instructor.teach

--- people.rb
+++ people.rb
@@ -1,7 +1,23 @@
 class Student
 
+  def initialize
+
+  end
+
+  def learn
+    puts "I get it!"
+  end
+
 end
 
 class Instructor
+
+  def initialize
+
+  end
+
+  def teach
+    puts "Everything in Ruby is an object."
+  end
 
 end
