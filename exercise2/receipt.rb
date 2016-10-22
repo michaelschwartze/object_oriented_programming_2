51e4e9851deb3471a87c93ce0dd1ba1d237fef26
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 13:48:09 -0400

    Change initialize, print_items; add print_total, print_tax; add attr_reader; style fixes

--- exercise2/receipt.rb
+++ exercise2/receipt.rb
@@ -1,24 +1,30 @@
 require_relative 'item'
 
+# Receipt class
 class Receipt
-  def initialize(item)
-    @items = [item]
+  attr_reader :items
+
+  def initialize(qty, item)
+    @items = []
+    add_item(qty, item)
   end
 
-  def add_item(item)
-    @items.push(item)
+  def add_item(qty, item)
+    @items.push([qty, item])
   end
 
   def print_items
-    @items.each do |x|
-      puts x.item_details
+    @items.each do |qty, item|
+      puts "#{qty} #{item.item_details}"
     end
   end
 
   def print_tax
+    puts 'Sales Taxes: '
   end
 
   def print_total
+    puts 'Total: '
   end
 
   def create_receipt
