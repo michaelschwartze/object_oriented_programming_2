9847db8eb9cd1e09fa1cfa471e878376320d6883
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 14:52:26 -0400

    Change array to have 1 qty each; change output

--- exercise2/main.rb
+++ exercise2/main.rb
@@ -10,15 +10,21 @@ item4 = Item.new('box of imported chocolates', 11.25, true, true)
 r1 = Receipt.new(1, item1)
 
 # Put remaining items in array
-myarray = [[5, item2], [6, item3], [1, item4]]
+myarray = [[1, item2], [1, item3], [1, item4]]
 
 # Put items in array into the receipt
 myarray.each do |qty, item|
   r1.add_item(qty, item)
 end
 
+r1.print_items
+r1.print_tax
+
 # Create receipt output
-r1.create_receipt
+# r1.create_receipt
+
+# r1.sum_price
+
 # Example 3:
 # 1 imported bottle of perfume at 27.99
 # 1 bottle of perfume at 18.99
