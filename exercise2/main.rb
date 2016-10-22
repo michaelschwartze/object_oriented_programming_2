4e368468ffd88cbfee60e2fa919c1def8855d8ec
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 10:15:27 -0400

    Create new Receipt with single item, add remaining items to array, add all items in array to recepit, print_items

--- exercise2/main.rb
+++ exercise2/main.rb
@@ -1,15 +1,24 @@
 require_relative 'receipt'
 
+#Create items
 item1 = Item.new('imported bottle of perfume', 27.99, true)
 item2 = Item.new('bottle of perfume', 18.99)
 item3 = Item.new('packet of headache pills', 9.75)
 item4 = Item.new('box of imported chocolates', 11.25, true)
-myarray = [item2, item3, item4]
+
+#Create new receipt r1 and add item1
 r1 = Receipt.new(item1)
 
-# myarray.each do |x|
-#   r1.add_item(x)
-# end
+#Put remaining items in array
+myarray = [item2, item3, item4]
+
+#Put items in array into the receipt
+myarray.each do |item|
+  r1.add_item(item)
+end
+
+#Print items in array r1
+r1.print_items
 
 # Example 3:
 # 1 imported bottle of perfume at 27.99
