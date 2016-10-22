7050e0feb57e6e9de0cd93b7da7725ef283c882e
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 13:41:46 -0400

    Change sequence of creating myarray and pushing items to myarray; rem r1.print_items add r1.create_receipt

--- exercise2/main.rb
+++ exercise2/main.rb
@@ -1,25 +1,24 @@
 require_relative 'receipt'
 
-#Create items
+# Create items
 item1 = Item.new('imported bottle of perfume', 27.99, true)
 item2 = Item.new('bottle of perfume', 18.99)
-item3 = Item.new('packet of headache pills', 9.75)
-item4 = Item.new('box of imported chocolates', 11.25, true)
+item3 = Item.new('packet of headache pills', 9.75, false, true)
+item4 = Item.new('box of imported chocolates', 11.25, true, true)
 
-#Create new receipt r1 and add item1
-r1 = Receipt.new(item1)
+# Create new receipt r1 and add item1
+r1 = Receipt.new(1, item1)
 
-#Put remaining items in array
-myarray = [item2, item3, item4]
+# Put remaining items in array
+myarray = [[5, item2], [6, item3], [1, item4]]
 
-#Put items in array into the receipt
-myarray.each do |item|
-  r1.add_item(item)
+# Put items in array into the receipt
+myarray.each do |qty, item|
+  r1.add_item(qty, item)
 end
 
-#Print items in array r1
-r1.print_items
-
+# Create receipt output
+r1.create_receipt
 # Example 3:
 # 1 imported bottle of perfume at 27.99
 # 1 bottle of perfume at 18.99
@@ -34,3 +33,9 @@ r1.print_items
 # 1 imported box of chocolates: 11.85
 # Sales Taxes: 6.70
 # Total: 74.68
+#
+# Current Output:
+# 1 imported bottle of perfume at 32.19 <= GOOD
+# 5 bottle of perfume at 20.89 <= GOOD
+# 6 packet of headache pills at 10.73 <= WRONG
+# 1 box of imported chocolates at 12.94 <= WRONG
