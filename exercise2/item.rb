c3a083d7cafd786eda36f59771336159809a03ee
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 23:05:20 -0400

    Add instance variables calc'd by instance methods import_tax, sales_tax, total_tax, total_price

--- exercise2/item.rb
+++ exercise2/item.rb
@@ -1,28 +1,40 @@
 # Item Class
 class Item
-  attr_reader :description, :price
-  attr_accessor :imported, :tax_exempt
+  attr_reader :description
+  attr_reader :price
+  attr_reader :imported
+  attr_reader :tax_exempt
+  attr_reader :import_tax
+  attr_reader :sales_tax
+  attr_reader :total_tax
+  attr_reader :total_price
 
   def initialize(description, price, imported = false, tax_exempt = false)
     @description = description
     @price = price
     @imported   = imported
     @tax_exempt = tax_exempt
-    @import_duty = import_duty
-    @item_tax = item_tax
+    #Calculated instance varaiables
+    @import_tax = import_tax
+    @sales_tax = sales_tax
+    @total_tax = total_tax
+    @total_price = total_price
   end
 
-  def import_duty
+  def import_tax
     @imported == true ? (@price * 0.05) : 0
   end
 
-  def item_tax
+  def sales_tax
     @tax_exempt == true ? 0 : (@price * 0.10)
   end
 
-  def item_price
-    item_total = @price + import_duty + item_tax
-    item_total.round(2)
+  def total_tax
+    @import_tax + @sales_tax
+  end
+
+  def total_price
+    @price + @total_tax
   end
 
   def item_details
