10e387cc6c8ba14503bb041c0d1b9b065ed0d20a
Michael Schwartze <mschwartze@gmail.com>
2016-10-22 23:19:38 -0400

    Delete methods, move calcs into init function

--- exercise2/item.rb
+++ exercise2/item.rb
@@ -2,10 +2,6 @@
 class Item
   attr_reader :description
   attr_reader :price
-  attr_reader :imported
-  attr_reader :tax_exempt
-  attr_reader :import_tax
-  attr_reader :sales_tax
   attr_reader :total_tax
   attr_reader :total_price
 
@@ -14,27 +10,10 @@ class Item
     @price = price
     @imported   = imported
     @tax_exempt = tax_exempt
-    #Calculated instance varaiables
-    @import_tax = import_tax
-    @sales_tax = sales_tax
-    @total_tax = total_tax
-    @total_price = total_price
-  end
-
-  def import_tax
-    @imported == true ? (@price * 0.05) : 0
-  end
-
-  def sales_tax
-    @tax_exempt == true ? 0 : (@price * 0.10)
-  end
-
-  def total_tax
-    @import_tax + @sales_tax
-  end
-
-  def total_price
-    @price + @total_tax
+    @import_tax = @imported == true ? (@price * 0.05) : 0
+    @sales_tax = @tax_exempt == true ? 0 : (@price * 0.10)
+    @total_tax = @import_tax + @sales_tax #total_tax
+    @total_price = @price + @total_tax
   end
 
   def item_details
