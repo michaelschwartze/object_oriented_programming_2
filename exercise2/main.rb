e527210b0c396356c4d754ba89f97adf29d907fd
Michael Schwartze <mschwartze@gmail.com>
2016-10-23 22:57:26 -0400

    Add checkout question and loop for items and print_receipt

--- exercise2/main.rb
+++ exercise2/main.rb
@@ -6,32 +6,16 @@ item2 = Item.new('bottle of perfume', 18.99)
 item3 = Item.new('packet of headache pills', 9.75, false, true)
 item4 = Item.new('box of imported chocolates', 11.25, true, true)
 
-r1 = Receipt.new
-r1.add_item(1, item1)
-r1.add_item(1, item2)
-r1.add_item(1, item3)
-r1.add_item(1, item4)
+myarray = [item1, item2, item3, item4]
 
-r1.print_receipt
+puts "Checkout with items in cart? (Y/N):"
 
+answer = gets.chomp.upcase
 
-# Example 3:
-# 1 imported bottle of perfume at 27.99
-# 1 bottle of perfume at 18.99
-# 1 packet of headache pills at 9.75
-# 1 box of imported chocolates at 11.25
-#
-#
-# Output:
-# 1 imported bottle of perfume: 32.19
-# 1 bottle of perfume: 20.89
-# 1 packet of headache pills: 9.75
-# 1 imported box of chocolates: 11.85
-# Sales Taxes: 6.70
-# Total: 74.68
-#
-# Current Output:
-# 1 imported bottle of perfume at 32.19 <= GOOD
-# 5 bottle of perfume at 20.89 <= GOOD
-# 6 packet of headache pills at 10.73 <= WRONG
-# 1 box of imported chocolates at 12.94 <= WRONG
+if answer == "Y"
+  r1 = Receipt.new
+  myarray.each do | item |
+    r1.add_item(1, item)
+  end
+  r1.print_receipt
+end
